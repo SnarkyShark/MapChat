c7b9b20b5b6177b5435d3f1d2455e4cc6e7ec622
Will Johannesen <willjohannesen@gmail.com>
2019-03-30 14:38:07 -0400

    set username + basic styling implemented

--- app/src/main/java/edu/temple/mapchat/MainActivity.java
+++ app/src/main/java/edu/temple/mapchat/MainActivity.java
@@ -2,12 +2,47 @@ package edu.temple.mapchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // on screen elements
+    EditText usernameEditText;
+    Button usernameButton;
+
+    // important stored values
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // hooking up screen elements
+        usernameEditText = findViewById(R.id.usernameEditText);
+        usernameButton = findViewById(R.id.usernameButton);
+
+        // set username
+        usernameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUsername();
+            }
+        });
+
+    }
+
+    /**
+     * Set username
+     * Generate public & private keys
+     */
+    private void setUsername() {
+        username = usernameEditText.getText().toString();
+        if (username.compareTo("") == 0)
+            username = "default";
+        setTitle("username: " + username);
+        //mService.genMyKeyPair(username);
     }
 }
