22579a882a5fd30ed064f0a540d115df5ca61681
Will Johannesen <willjohannesen@gmail.com>
2019-04-10 10:20:45 -0400

    Lab 2 portion up and running

--- app/src/main/java/edu/temple/mapchat/MainActivity.java
+++ app/src/main/java/edu/temple/mapchat/MainActivity.java
@@ -1,17 +1,27 @@
 package edu.temple.mapchat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +33,12 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -30,22 +46,20 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, NfcAdapter.CreateNdefMessageCallback {
-
-    // list of friends
-    ListView listView;
-    ArrayList<String> friends;
-    public static final String EXTRA_FRIEND = "";
 
     // username
     String username;
@@ -56,15 +70,34 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     Context context;
 
     // map
-    private GoogleMap theMap;
+    private LocationManager lm;
+    private LocationListener ll;
+    private GoogleMap mMap;
+    private Marker lastMarker;
+    private Location mLocation;
+    private static final String LOC_LAT = "LAST_LAT";
+    private static final String LOC_LNG = "LAST_LNG";
+    private static final String LOC_PRO = "LAST_PRO";
+
+    // list of friends
+    ListView listView;
+    ArrayAdapter<String> friendNamesAdapter;
+    ArrayList<String> friendNames;
+    public static final String EXTRA_FRIEND = "";
+
+    // api
+    private RequestQueue queue;
+    ArrayList<Friend> friends;
+    private String getFriendsUrl = "https://kamorris.com/lab/get_locations.php";
+    private String postPosUrl = "https://kamorris.com/lab/register_location.php";
+
+    // Android Beam
+    NfcAdapter nfcAdapter;
+    private PendingIntent mPendingIntent;
 
     // service
     KeyService mService;
     boolean mBound = false;
-
-    // NFC Beam
-    NfcAdapter nfcAdapter;
-    private PendingIntent mPendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +111,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // initialize values
         friends = new ArrayList<>();
+        friendNames = new ArrayList<>();
         sharedPref = getSharedPreferences("myMapChatApp", Context.MODE_PRIVATE);
 
         // check/set username
@@ -95,12 +129,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         transaction.replace(R.id.mapView, mapFragment).commit();
         mapFragment.getMapAsync(MainActivity.this);
 
+        lm = getSystemService(LocationManager.class);
+        ll = makeLocationListener();
+
+        String lat = sharedPref.getString(LOC_LAT, null);
+        String lon = sharedPref.getString(LOC_LNG, null);
+        mLocation = null;
+        if (lat != null && lon != null) {
+            String provider = sharedPref.getString(LOC_PRO, null);
+            mLocation = new Location(provider);
+            mLocation.setLatitude(Double.parseDouble(lat));
+            mLocation.setLongitude(Double.parseDouble(lon));
+        }
+        if (mLocation != null && mMap != null)
+            displayYourPin(mLocation);
+
+
         // list of friends
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, friends);
-        listView.setAdapter(adapter);
+        friendNamesAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, friendNames);
+        listView.setAdapter(friendNamesAdapter);
         listView.setOnItemClickListener(messageClickedHandler);
-        getFriends();
+
+        // api
+        queue = Volley.newRequestQueue(this);
+        //get();
 
         // Android Beam
         Intent nfcIntent = new Intent(this, MainActivity.class);
@@ -116,7 +169,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
      * Set username
      * Generate public & private keys
      */
-
     private void checkUsername() {
         String storedUsername = sharedPref.getString(USER_PREF_KEY, null);
         if(storedUsername != null) {
@@ -151,33 +203,150 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     /**
+     * Get and Post Partner Data
+     */
+    private void get() {
+        JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.GET, getFriendsUrl, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        int len = response.length();
+                        friends = new ArrayList<>();
+                        for(int i=0; i<len; i++) {
+                            String user = "";
+                            double lat = 0, lng = 0;
+                            try {
+                                JSONObject jresponse = response.getJSONObject(i);
+                                user = jresponse.getString("username");
+                                lat = jresponse.getDouble("latitude");
+                                lng = jresponse.getDouble("longitude");
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                Log.e(" friendtest", "json issue");
+                            }
+
+                            Friend newPal = new Friend(user, lat, lng);
+                            if (mLocation != null)
+                                newPal.calculateDist(mLocation.getLatitude(), mLocation.getLongitude());
+                            else
+                                newPal.calculateDist(lat, lng);
+
+                            friends.add(newPal);
+                            Log.e(" friendtest", "added friend: " + newPal + ": " + newPal.getDistance() + " away");
+
+                        }
+                        Log.e(" friendtest", "successful volley request");
+                        convertFriends(len);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(" friendtest", "volley request failed");
+            }
+        });
+        queue.add(getRequest);
+    }
+
+    public void convertFriends(int len) {
+        try {   // convert Friend arraylist to String arraylist
+            friendNames.clear();
+            Collections.sort(friends);
+            for(int i=0; i<len; i++) {
+                Log.e(" friendsort", "map: " + friends.get(i).toString());
+                friendNames.add(friends.get(i).toString());
+            }
+        } catch (Exception e) {
+            Log.e(" friendsort", "the toggle switch isn't toggling");
+        }
+        friendNamesAdapter.notifyDataSetChanged();
+    }
+
+    private void post() {
+
+    }
+
+    /**
      * Google Map View
      */
+    // track your current location
+    private LocationListener makeLocationListener(){
+        Log.e( "marktrack", "kicked off makeLocationlistener");
+        return new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.e( "marktrack", "location changed");
+                mLocation = location;
+                displayYourPin(location);
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+            @Override
+            public void onProviderEnabled(String provider) { }
+            @Override
+            public void onProviderDisabled(String provider) { }
+        };
+    }
+
+    private void displayYourPin(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        if (lastMarker != null) {
+            lastMarker.setPosition(latLng);
+        }
+        else {
+            MarkerOptions markerOptions = (new MarkerOptions())
+                    .position(latLng)
+                    .title("You");
+
+            lastMarker = mMap.addMarker(markerOptions);
+            Log.e( "marktrack", "added a marker");
+
+        }
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory
+                .newLatLngZoom(lastMarker.getPosition(), 14);
+
+        mMap.moveCamera(cameraUpdate);
+        Log.e( "marktrack", "tried to do camera stuff");
+    }
+
     @Override
     public void onMapReady(GoogleMap map) {
-        theMap = map;
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        theMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        theMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap = map;
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e(" map", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(" map", "Can't find style. Error: ", e);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            registerForLocationUpdates();
+        else
+            Toast.makeText(this, "No map permission", Toast.LENGTH_LONG).show();
+    }
+    @SuppressLint("MissingPermission")
+    private void registerForLocationUpdates() {
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, ll);
+        lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, ll);
     }
 
     /**
      * Friends ListView
      */
-    public void getFriends() {
-
-        friends.add("ron");
-        friends.add("paul");
-        friends.add("blart");
-        friends.add("mall");
-        friends.add("cop");
-        friends.add("drop");
-        friends.add("top");
-        friends.add("crop");
-        friends.add("mop");
-    }
-
     // Launch ChatActivity
     private AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
         public void onItemClick(AdapterView parent, View v, int position, long id) {
@@ -258,19 +427,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onResume();
         Log.e( " beamtrack", "We resumed");
 
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            registerForLocationUpdates();
+        else
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 111); // make a constant reference
+
         // Get the intent from Beam
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
             Log.e( " beamtrack", "We discovered an NDEF");
             processIntent(getIntent());
         }
         nfcAdapter.enableForegroundDispatch(this, mPendingIntent, null, null);
+
+        get();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
+        lm.removeUpdates(ll);
         nfcAdapter.disableForegroundDispatch(this);
+        if (mLocation != null) {
+            sharedPref.edit().putString(LOC_LAT, String.valueOf(mLocation.getLatitude())).apply();
+            sharedPref.edit().putString(LOC_LNG, String.valueOf(mLocation.getLongitude())).apply();
+            sharedPref.edit().putString(LOC_PRO, mLocation.getProvider()).apply();
+
+        }
     }
 
     @Override
